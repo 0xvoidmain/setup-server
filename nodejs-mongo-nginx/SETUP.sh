1096863d6c5dc1a16c5386bfebe3b76dc005c7e6
tunght91 <hoangtung.utc@gmail.com>
2023-03-14 23:41:44 +0700

    update

--- nodejs-mongo-nginx/SETUP.sh
+++ nodejs-mongo-nginx/SETUP.sh
@@ -5,7 +5,20 @@ echo "-------- INSTALL NODEJS --------"
 curl -sL https://deb.nodesource.com/setup_14.x | sudo -E bash -
 sudo apt-get install -y nodejs
 sudo apt-get install -y build-essential
-apt install npm
+
+# INSTALL MONGODB
+echo "-------- INSTALL MONGODB --------"
+wget -qO - https://www.mongodb.org/static/pgp/server-5.0.asc | sudo apt-key add -
+echo "deb [ arch=amd64,arm64 ] https://repo.mongodb.org/apt/ubuntu focal/mongodb-org/5.0 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-5.0.list
+sudo apt-get update
+sudo apt-get install -y mongodb-org
+
+echo "-------- RUN MONGODB --------"
+mkdir /data
+mkdir /data/db
+sudo systemctl daemon-reload
+sudo systemctl enable mongod
+sudo systemctl start mongod
 
 # INSTALL PM2
 echo "-------- INSTALL PM2 --------"
